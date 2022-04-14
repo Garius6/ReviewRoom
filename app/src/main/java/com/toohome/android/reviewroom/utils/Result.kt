c0225a987b2de65ca03a68bfbd75638e72ff96eb
Gaidar Musaev <gaidarforprivateusege@gmail.com>
2022-04-14 16:33:52 +0300

    Added new state for query
    Signed-off-by: Gaidar Musaev <gaidarforprivateusege@gmail.com>

--- app/src/main/java/com/toohome/android/reviewroom/utils/Result.kt
+++ app/src/main/java/com/toohome/android/reviewroom/utils/Result.kt
@@ -2,6 +2,8 @@ package com.toohome.android.reviewroom.utils
 
 sealed class Result<out Success, out Failure>
 
+object PendingResult : Result<Nothing, Nothing>()
+
 data class SuccessResult<out Success>(
     val data: Success
 ) : Result<Success, Nothing>()
